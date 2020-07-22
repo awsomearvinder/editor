b1b051c0b45b3cb1315f29254c34884f3fa98d3b
Arvinder Dhanoa <ArvinderDhan@gmail.com>
2020-07-22 17:23:21 -0500

    fix some documentation and clean the new() method

--- src/bridge/mod.rs
+++ src/bridge/mod.rs
@@ -6,6 +6,8 @@
  */
 
 use crate::ui::Msg;
+//I doubt we plan on using a traditional std::sync::Mutex, so I believe confusion won't exist.
+use async_mutex::Mutex;
 use futures::executor;
 use nvim_rs::{compat::tokio::Compat, create::tokio as create, neovim::Neovim};
 use std::{
@@ -28,9 +30,9 @@ type MyNeovim = Neovim<Compat<ChildStdin>>;
 const FONTSIZE: u32 = 16;
 
 pub struct Bridge {
-    nvim_session: Arc<async_mutex::Mutex<MyNeovim>>,
+    nvim_session: Arc<Mutex<MyNeovim>>,
     already_attached_ui: bool,
-    pub rx: Arc<async_mutex::Mutex<UnboundedReceiver<Msg>>>,
+    pub rx: Arc<Mutex<UnboundedReceiver<Msg>>>,
 }
 
 impl Debug for Bridge {
@@ -46,22 +48,18 @@ impl Bridge {
     //It only runs once. I don't give  a shit.
     pub fn new() -> Self {
         let (tx, rx) = mpsc::unbounded_channel();
-        let neovim = Arc::new(async_mutex::Mutex::new(
-            executor::block_on(async {
-                create::new_child(Handler(Arc::new(async_mutex::Mutex::new(tx)))).await
-            })
-            .unwrap()
-            .0,
-        ));
-        executor::block_on(async { neovim.lock().await.subscribe("buf").await }).unwrap();
+
+        let (neovim, _, _) =
+            executor::block_on(async { create::new_child(Handler::new(tx)).await }).unwrap();
+
         Self {
-            nvim_session: neovim,
+            nvim_session: Arc::new(Mutex::new(neovim)),
             already_attached_ui: false,
-            rx: std::sync::Arc::new(async_mutex::Mutex::new(rx)),
+            rx: Arc::new(Mutex::new(rx)),
         }
     }
 
-    ///Changes the resolution reported to neovim - this is a requirement.
+    ///Changes the resolution reported to neovim - this is a requirement imposed by neovim itself.
     pub fn change_resolution(&mut self, width: u32, height: u32) -> iced::Command<Msg> {
         let nvim_session = self.nvim_session.clone();
 
@@ -128,8 +126,21 @@ impl Bridge {
     }
 }
 
+///This is what recives messages from neovim. look in handle_request
+///and handle_notify in nvim_rs::Handler for more information.
 #[derive(Clone)]
-struct Handler(Arc<async_mutex::Mutex<UnboundedSender<Msg>>>);
+struct Handler {
+    sender_channel: Arc<Mutex<UnboundedSender<Msg>>>,
+}
+
+impl Handler {
+    fn new(sender: UnboundedSender<Msg>) -> Self {
+        Self {
+            sender_channel: Arc::new(Mutex::new(sender)),
+        }
+    }
+}
+
 #[async_trait::async_trait]
 impl nvim_rs::Handler for Handler {
     type Writer = Compat<ChildStdin>;
@@ -148,7 +159,7 @@ impl nvim_rs::Handler for Handler {
 
     async fn handle_notify(&self, _name: String, args: Vec<rmpv::Value>, _neovim: MyNeovim) {
         //TODO: Need to figure out how message sending works. T is an enum we send?
-        let _channel = self.0.lock().await;
+        let _channel = self.sender_channel.lock().await;
         for arg in args {
             if !arg.is_array() {
                 break;
